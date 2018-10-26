d710a56175acccb7b5e59e9b9a7b46eb79eefa89
lokkyX3 <ting_twl.X3@live.com>
2018-10-26 11:55:38 +0800

    Minor cleanup

--- reddit-scrap.py
+++ reddit-scrap.py
@@ -16,13 +16,13 @@ reddit = praw.Reddit(
                      client_secret='',
                      username='',
                      password='',
-                     user_agent='',
+                     user_agent=''
                      )
 
 #writing retrieved list of posts into csv
-def write_csv(to_csv, sub):
+def write_csv(to_csv, csv_name):
     try:
-        with open(sub+'_'+datetime.datetime.now().strftime('%Y%m%d')+'.csv', 'w', newline='', encoding='utf-8') as file:
+        with open(csv_name, 'w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
             writer.writerows(to_csv)
             file.close()
@@ -30,7 +30,7 @@ def write_csv(to_csv, sub):
         print(e)
 
 #retrieve post from reddit ~ duh
-def retrieve_post(sub,post_number):
+def retrieve_post(sub,post_number,csv_name):
     subreddit = reddit.subreddit(sub)
     sub_hot = subreddit.hot(limit=post_number)
     to_csv = [['title', 'score', 'comment_count', 'author', 'nsfw', 'upvote_ratio', 'post_date']]
@@ -48,16 +48,18 @@ def retrieve_post(sub,post_number):
                             datetime.datetime.fromtimestamp(post.created).strftime('%Y-%m-%d %H:%M')
                             ]
                 to_csv.append(retrieved)
-        write_csv(to_csv, sub)
+        write_csv(to_csv, csv_name)
     except Exception as e:
         print(e)
 
 for sub in sub_name:
+    #Note: The csv file names are saved in {subreddit name}_yyyymmdd.csv format
+    csv_name = '{}_{}.csv'.format(sub,datetime.datetime.now().strftime("%Y%m%d"))
     #csv path
-    csv_file = Path('{}_{}.csv'.format(sub,datetime.datetime.now().strftime("%Y%m%d"))) #Note: The csv file names are saved in {subreddit name}_yyyymmdd.csv format
+    csv_file = Path(csv_name)
 
     #check if csv file already exist to prevent overwriting before retrieving post from reddit
     if csv_file.exists():
-        print('File {}_{}.csv already exist'.format(sub,datetime.datetime.now().strftime("%Y%m%d")))
+        print('File {} already exist!'.format(csv_name))
     else:
-        retrieve_post(sub,post_num)
+        retrieve_post(sub,post_num,csv_name)
